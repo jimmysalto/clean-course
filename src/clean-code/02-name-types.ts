c24c9ad57d2c83a070edb4835e3b54c793522eb1
Fernando <saltosumbafernando@gmail.com>
2023-12-26 15:31:22 -0500

    ADD: name-types in clen code reviewed

---
+++ src/clean-code/02-name-types.ts
@@ -0,0 +1,44 @@
+(() => {
+
+    const temperaturesCelsius = [33.6, 12.34];
+
+    const serverIp = '123.123.123.123';
+
+    const users = [
+        { id: 1, email: 'fernando@google.com' },
+        { id: 2, email: 'juan@google.com' },
+        { id: 3, email: 'melissa@google.com' }
+    ];
+
+    const emailUsers = users.map(user => user.email);
+
+    const canJump = false;
+    const canRun = true;
+    const hasItems = true;
+    const isLoading = false;
+
+    const startTime = new Date().getTime();
+
+    const endTime = new Date().getTime() - startTime;
+
+    function getBooks() {
+        throw new Error('Function not implemented.');
+    }
+
+    function getBookByUrl(url: string) {
+        throw new Error('Function not implemented.');
+    }
+
+    function getSquareArea(side: number) {
+        throw new Error('Function not implemented.');
+    }
+
+    function printJob() {
+        throw new Error('Function not implemented.');
+    }
+
+})();
+
+
+
+
