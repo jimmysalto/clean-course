6c3a6aff454321bfa36722c45a73712113c2b133
Fernando <saltosumbafernando@gmail.com>
2023-12-27 12:29:54 -0500

    ADD: Bad code

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -1,7 +1,6 @@
 (() => {
 
-    
-    
+    // No aplicando el principio de responsabilidad única
     type Gender = 'M' | 'F';
 
     class Person {
@@ -10,12 +9,8 @@
             public name: string,
             public gender: Gender,
             public birthdate: Date
-        ) {
-            this.name = name;
-            this.gender = gender;
-            this.birthdate = birthdate;
-        }
-
+        ) { }
+        
     }
 
     const newPerson = new Person(
@@ -25,5 +20,66 @@
     );
 
     console.log({ newPerson });
+
+    class User extends Person {
+
+        public lastAccess: Date;
+
+        constructor(
+            public email: string,
+            public role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date
+        ) {
+            super(
+                name,
+                gender,
+                birthdate
+            );
+            this.lastAccess = new Date();
+        }
+
+        checkCredentials() {
+            console.log('Checking credentials...');
+            return true;
+        }
+    }
+
+    class UserSettings extends User {
+        constructor(
+            public workingDirectory: string,
+            public lastOpenFolder: string,
+            email: string,
+            role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date
+
+        ) {
+            super(
+                email,
+                role,
+                name,
+                gender,
+                birthdate
+            );
+        }
+    }
+
+    const userSettings = new UserSettings(
+        '/home/jimmy',
+        '/home/jimmy/projects',
+        'saltojimmy@gmail.com',
+        'admin',
+        'Jimmy',
+        'M',
+        new Date('1996-04-20')
+    );
+
+    console.log({
+        userSettings,
+        areCredentialsValid: userSettings.checkCredentials()
+    });
 
 })();
